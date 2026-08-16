aab3824f8f3aa6a12c5ec9810117225ff057605c
zuhaibakhtarkhan <zuhaibakhtarkhan@gmail.com>
2026-08-17 02:33:37 +0500

    The initial mongodb settings

---
+++ cleanup.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+source .env.network
+source .env.volume
+source .env.db
+
+if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
+    echo "Removing container $CONTAINER_NAME"
+    docker stop $CONTAINER_NAME
+    # docker rm $CONTAINER_NAME
+else
+    echo "A container with the name $CONTAINER_NAME does not exist. Skipping container deletion."
+fi
+
+
+if [ "$(docker volume ls -q -f name=$VOLUME_NAME)" ]; then
+    echo "Removing volume $VOLUME_NAME"
+    docker volume rm $VOLUME_NAME
+else
+    echo "A volume with the name $VOLUME_NAME does not exist. Skipping volume deletion."
+fi
+
+if [ "$(docker network ls -q -f name=$NETWORK_NAME)" ]; then
+    echo "Removing network $NETWORK_NAME"
+    docker network rm $NETWORK_NAME
+else
+    echo "A network with the name $NETWORK_NAME does not exist. Skipping network deletion."
+fi
