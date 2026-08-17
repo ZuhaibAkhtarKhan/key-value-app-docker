8de4721528150e38e03d7ffdba80defe82019faf
zuhaibakhtarkhan <zuhaibakhtarkhan@gmail.com>
2026-08-17 18:31:14 +0500

    Make backend using shell scripting and docker

---
+++ backend/src/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/health', (req, res) => {
+    res.status(200).send('up');
+})
+
+console.log('Connecting to DB');
+mongoose.connect(`mongodb://${process.env.MONGODB_HOST}/${process.env.KEY_VALUE_DB}`, {
+    auth: {
+        username: process.env.KEY_VALUE_USER,
+        password: process.env.KEY_VALUE_PASSWORD,
+    },
+    connectTimeoutMS: 500
+}).then(() => {
+    app.listen(process.env.CONTAINER_PORT, () => {
+        console.log(`Listening on Port ${process.env.CONTAINER_PORT}.`);
+    })
+    console.log('Connected to DB')
+}).catch(err => {
+    console.log('Something went wrong!');
+    console.log(err);
+});
+
